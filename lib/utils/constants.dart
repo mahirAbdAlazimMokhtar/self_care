c8bc1963936ce939c292bd1d57fd098ac4c424ca
Mahir <mahr.bwb@gmail.com>
2023-10-16 10:38:53 +0300

    project initial

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static Color grey = const Color(0xfff6f6f6);
+  static Color green = const Color(0xff299272);
+  static Color blackGreen = const Color(0xff1c2d27);
+  static List<Color> practiceTileColors = [
+    const Color(0xffc4f1cd),
+    const Color(0xffe5eec2),
+    const Color(0xffaaede9),
+    const Color(0xffbfe9ef),
+    const Color(0xffdff1d9),
+    const Color(0xffc0ffb1),
+  ];
+}
