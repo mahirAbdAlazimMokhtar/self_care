8debe560a14fb889c3169cc2776c08fd7eeff216
Mahir <mahr.bwb@gmail.com>
2023-10-17 07:29:13 +0300

    add the top section 👏✅

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,112 @@
+import 'package:badges/badges.dart' as badges;
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:self_care/utils/constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _slelectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var hight = MediaQuery.of(context).size.height;
+    List<String> selfCareItems = ['Practice', 'Movies', 'Books', 'Learning'];
+    List<String> practiceItems = [
+      'Meditation',
+      'Wake Up',
+      'Positive Focus',
+      'Deep Breathing',
+      'Sleep',
+      'Creativity'
+    ];
+    List<String> practiceEmoji = [
+      '🧘',
+      '🛏️',
+      '😮‍💨',
+      '👁️',
+      '🎨',
+    ];
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Stack(children: [
+          Column(children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 30),
+                      Row(
+                        children: [
+                          Container(
+                            decoration: ShapeDecoration(
+                                shape: ContinuousRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                color: Constants.grey),
+                            padding: const EdgeInsets.all(20),
+                            child: const Icon(Icons.person),
+                          ),
+                          const SizedBox(width: 10),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Mahir Mokhtar',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
+                              SizedBox(height: 3),
+                              Text('Patient',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 13)),
+
+
+                            ],
+                          ),
+                          const Spacer(),
+                          Container(
+                            decoration: ShapeDecoration(
+                              shape:ContinuousRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              color: Constants.grey,
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: badges.Badge(
+                              position: badges.BadgePosition.topEnd(
+                                top: 0,
+                                end: 2,
+                              ),
+                              badgeStyle: badges.BadgeStyle(
+                                badgeColor: Constants.green,
+                                borderSide: const BorderSide(
+                                  color: Colors.white,
+                                  width: 1.5,
+                                ),
+                              ),
+                              child: const Icon(
+                                  Iconsax.notification5,
+                                  color:Colors.grey
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )),
+            ),
+          ])
+        ])));
+  }
+}
